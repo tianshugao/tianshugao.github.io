7f221b5b1ad2a3b6342edb79a05a563290639543
tianshugao <54757223+tianshugao@users.noreply.github.com>
2019-12-05 14:41:58 +0800

    Add files via upload

--- website/js/agency.js
+++ website/js/agency.js
@@ -15,6 +15,21 @@
     }
   });
 
+
+  // Back to top button  
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+  $('.back-to-top').click(function(){
+    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+    return false;
+  });
+
+  
   // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function() {
     $('.navbar-collapse').collapse('hide');
